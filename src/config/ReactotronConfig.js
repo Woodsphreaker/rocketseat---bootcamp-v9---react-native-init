6ac384629916ed5d85fde15f07ca5d9d3e51accb
Carlo Enrico <carlo.enrico@uol.com.br>
2019-12-27 15:05:35 -0300

    Initial commit

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,9 @@
+import Reactotron from 'reactotron-react-native'
+
+if (__DEV__) {
+  const tron = Reactotron.configure({host: '10.1.1.10'})
+    .useReactNative()
+    .connect()
+  console.tron = tron
+  tron.clear()
+}
